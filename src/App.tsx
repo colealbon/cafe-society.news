3b93d851fbabc2ebc11d10038d77a49170ee3e58
Cole Albon <cole@cafe-society.news>
2023-08-16 17:44:57 -0700

    swede some animations

--- src/App.tsx
+++ src/App.tsx
@@ -20,15 +20,13 @@ function createStoredSignal<T>(
 }
 
 const App: Component = () => {
-  const navBarWidth = '1/3'
-  const navButtonStyle=`m-1 w-${navBarWidth} bg-transparent border-none transition-all bg-transparent border-none hover-text-white hover:bg-slate-300 rounded`
-
+  const navButtonStyle=`text-xl text-white border-none transition-all bg-transparent hover-text-white hover:bg-slate-600 rounded`
   const [navIsOpen, setNavIsOpen] = createStoredSignal('isNavOpen', false);
   return (
     <div>
-      <div class={navIsOpen() ? 'hidden' : ''}>
+      <div class={navIsOpen() ? 'm-1 hidden' : 'm-1 animate-fade-in-right animate-duration-.3s '}>
         <Button.Root
-          class={`transition-all bg-transparent border-none hover-text-white hover:bg-slate-300 rounded-full`}
+          class={`text-4xl transition-all bg-transparent border-none hover-text-white hover:bg-slate-600 rounded-full`}
           onClick={event => {
             event.preventDefault()
             setNavIsOpen(true)
@@ -36,45 +34,37 @@ const App: Component = () => {
         >⭢
         </Button.Root>
       </div>
-      <div class={navIsOpen() ? '' : 'hidden'}>
-        <Button.Root
-          class={`transition-all bg-transparent border-none hover-text-white hover:bg-slate-300 rounded-full`}
-          onClick={event => {
-            event.preventDefault()
-            setNavIsOpen(false)
-          }}
-        >
-          ⭠
-        </Button.Root>
-      </div>
-      <Tabs.Root orientation='vertical'>
-      <div class='flex flex-row'>
-        <div class={navIsOpen() ? '' : 'hidden'}>
-          <div>
-            <Tabs.List>
-              <div>
-                <Tabs.Trigger class={navButtonStyle} value="profile">Profile</Tabs.Trigger>
+      <Tabs.Root>
+        <div class='m-1 flex flex-column'>
+          <div class={navIsOpen() ? `animate-fade-in-left animate-duration-.3s` : 'hidden'}>
+            <div class={`bg-black w-full h-9/10 rounded-2 mr-3`}>
+              <div class={navIsOpen() ? '' : 'animate-fade-out-left animate-duration-.3s'}>
+                <Button.Root
+                  class={`text-4xl text-white transition-all bg-transparent border-none hover-text-white hover:bg-slate-600 rounded-full`}
+                  onClick={event => {
+                    event.preventDefault()
+                    setNavIsOpen(false)
+                  }}
+                >
+                  ⭠
+                </Button.Root>
               </div>
-              <div>
-                <Tabs.Trigger class={navButtonStyle} value="dashboard">Dashboard</Tabs.Trigger>
-              </div>
-              <div>
-                <Tabs.Trigger class={navButtonStyle} value="settings">Settings</Tabs.Trigger>
-              </div>
-              <div>
-                <Tabs.Trigger class={navButtonStyle} value="contact">Contact</Tabs.Trigger>
-              </div>
-            </Tabs.List>
+              <Tabs.List>
+                <div><Tabs.Trigger class={navButtonStyle} value="profile">Profile</Tabs.Trigger></div>
+                <div><Tabs.Trigger class={navButtonStyle} value="dashboard">Dashboard</Tabs.Trigger></div>
+                <div><Tabs.Trigger class={navButtonStyle} value="settings">Settings</Tabs.Trigger></div>
+                <div><Tabs.Trigger class={navButtonStyle} value="contact">Contact</Tabs.Trigger></div>
+              </Tabs.List>
+            </div>
           </div>
-        </div>
-        <div class='w-full transition-all'>
-          <div class='flex items-center justify-center m-5'>
-            <Tabs.Content value="profile">Profile details</Tabs.Content>
-            <Tabs.Content value="dashboard">Dashboard details</Tabs.Content>
-            <Tabs.Content value="settings">Settings details</Tabs.Content>
-            <Tabs.Content value="contact">Contact details</Tabs.Content>
+          <div class='w-full h-screen'>
+            <div class='flex justify-center m-5 h-9/10'>
+              <Tabs.Content class='animate-fade-in' value="profile">Profile details</Tabs.Content>
+              <Tabs.Content class='animate-fade-in' value="dashboard">Dashboard details</Tabs.Content>
+              <Tabs.Content class='animate-fade-in' value="settings">Settings details</Tabs.Content>
+              <Tabs.Content class='animate-fade-in' value="contact">Contact details</Tabs.Content>
+            </div>
           </div>
-        </div>
         </div>
       </Tabs.Root>
     </div>
